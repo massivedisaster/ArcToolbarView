a0b08d32d4c052f80b9a8233b50fb69904f936d8
jzeferino <jorgevalentzeferino@gmail.com>
2017-10-30 22:47:37 +0000

    Added other sample to show view capabilities.

--- sample/src/main/java/com/massivedisaster/sample/ArcToolbarViewActivity.kt
+++ sample/src/main/java/com/massivedisaster/sample/ArcToolbarViewActivity.kt
@@ -1,10 +1,8 @@
 package com.massivedisaster.sample
 
+import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import com.massivedisaster.widget.extensions.setAppBarLayout
 import kotlinx.android.synthetic.main.activity_arc_toolbar_view.*
 
@@ -14,9 +12,8 @@ class ArcToolbarViewActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_arc_toolbar_view)
         setSupportActionBar(toolbar)
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        fab.setOnClickListener { _ ->
+            startActivity(Intent(this, MainActivity::class.java))
         }
 
         arcToolbar.setAppBarLayout(appbar)
